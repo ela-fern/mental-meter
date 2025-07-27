f51d7814afd87fa4ee7aaa11fdf76193ad37ebeb
ela-fern <elamfernandez18@gmail.com>
2025-07-27 15:13:47 -0600

    Update database.py
    just undid the thing i did like 10 minutes ago

--- database.py
+++ database.py
@@ -6,13 +6,6 @@ from datetime import datetime, timedelta
 import pandas as pd
 import hashlib
 
-# create missing tables
-from sqlalchemy import create_engine
-from .models import Base  # or wherever your models are
-
-engine = create_engine(DATABASE_URL)
-Base.metadata.create_all(bind=engine)
-
 # Database setup
 DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
